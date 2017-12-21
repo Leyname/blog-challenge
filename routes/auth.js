467104f23178fdfce061fbd5eb3d8358c8b8e87d
lodoss <developer@iMac-lodoss.local>
2017-12-21 12:16:15 +0300

    Adding jwt authenticate

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,15 @@
 const authController = require('../controllers/auth');
 const router = require('express-promise-router')();
+const strategy = require('../common/auth_strategy');
+const passport = require('passport');
+
+passport.use(strategy);
 
 router.post('/registration', authController.registration);
 router.post('/auth/confirm/:hash_code', authController.confirmUser);
 router.post('/auth/login', authController.login);
 router.post('/auth/forgot_password', authController.changePassword);
 router.post('/auth/reset', authController.reset);
-router.get('/auth/check_code/:code', authController.checkCode);
+router.get('/auth/check_code/:code', passport.authenticate('jwt', { session: false }), authController.checkCode);
 
 module.exports = router;
