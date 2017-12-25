6ca0672d4eee0f69dffa0ffc6be586f2d6ddcabc
lodoss <developer@iMac-lodoss.local>
2017-12-25 12:35:08 +0300

    Adding sendEmail in mailer

--- common/mailer.js
+++ common/mailer.js
@@ -5,10 +5,29 @@ const mailerService = config.get('mailer.service');
 const mailerName = config.get('mailer.user');
 const mailerPass = config.get('mailer.pass');
 
-module.exports = nodemailer.createTransport({
+const smtpTransport = nodemailer.createTransport({
   service: mailerService,
   auth: {
     user: mailerName,
     pass: mailerPass,
   },
 });
+
+const sendEmail = async (email, link) => {
+  const mailOptions = {
+    to: email,
+    subject: 'Please confirm your Email account',
+    html: `Hello,<br> Please Click on the link to verify your email.<br><a href="${link}">Click here to verify</a>`,
+  };
+
+  await smtpTransport.sendMail(mailOptions, (error) => {
+    if (error) {
+      console.log(error);
+      throw { success: false, message: 'mail is not send' };
+    }
+  });
+};
+
+module.exports = {
+  sendEmail,
+};
