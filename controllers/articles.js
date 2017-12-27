e05eab51c5de6b24928c6098699e88b710594a44
lodoss <developer@iMac-lodoss.local>
2017-12-27 19:09:54 +0300

    Correction in getting id

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,36 +1,58 @@
 const articles = require('../models/article');
 const commentaries = require('../models/comment');
-const jwt = require('jsonwebtoken');
 
 const editArticle = async (req, res, next) => {
-  const { params: { id: articleId } } = req;
-  const token = req.headers.authorization.slice(4);
-  const decodedId = await jwt.decode(token);
+  const {
+    params: {
+      id: articleId,
+    },
+  } = req;
+  const {
+    user: {
+      id: userId,
+    },
+  } = req;
   const article = await articles.findArticleById(articleId);
 
   if (!article) {
-    throw { success: false, message: 'article is not found' };
+    throw {
+      success: false,
+      message: 'article is not found',
+    };
   }
 
-  if (article.author_id === decodedId.id) {
+  if (article.author_id === userId) {
     const affectedCount = await articles.editArticleById(req.body, articleId);
-    console.log(affectedCount);
+
     if (affectedCount[0] > 0) {
-      res.data = { success: true };
+      res.data = {
+        success: true,
+      };
       return next();
     }
 
-    throw { success: false, message: 'Editing in not done' };
+    throw {
+      success: false,
+      message: 'Editing in not done',
+    };
   } else {
-    throw { success: false, message: 'no rights to edit this article' };
+    throw {
+      success: false,
+      message: 'no rights to edit this article',
+    };
   }
 };
 
 const addArticle = async (req, res, next) => {
-  const token = req.headers.authorization.slice(4);
-  const decodedId = await jwt.decode(token);
-  await articles.addNewArticle(req.body, decodedId.id);
-  res.data = { success: true };
+  const {
+    user: {
+      id: userId,
+    },
+  } = req;
+  await articles.addNewArticle(req.body, userId);
+  res.data = {
+    success: true,
+  };
   next();
 };
 
@@ -46,84 +68,135 @@ const getPubicArticles = async (req, res, next) => {
 };
 
 const getMyArticles = async (req, res, next) => {
-  const token = req.headers.authorization.slice(4);
-  const decodedId = await jwt.decode(token);
+  const {
+    user: {
+      id: userId,
+    },
+  } = req;
   if ((Object.keys(req.query).length) === 0) {
-    const myArticles = await articles.getArticlesByAuthor(decodedId.id);
+    const myArticles = await articles.getArticlesByAuthor(userId);
     res.data = myArticles;
     next();
   }
-  const filteringArticles = await articles.filterArticlesByAuthor(req.query, decodedId.id);
+  const filteringArticles = await articles.filterArticlesByAuthor(req.query, userId);
   res.data = filteringArticles;
   next();
 };
 
 const deleteArticles = async (req, res, next) => {
-  const { params: { id: articleId } } = req;
-  const token = req.headers.authorization.slice(4);
-  const decodedId = await jwt.decode(token);
+  const {
+    params: {
+      id: articleId,
+    },
+  } = req;
+  const {
+    user: {
+      id: userId,
+    },
+  } = req;
   const article = await articles.findArticleById(articleId);
 
   if (!article) {
-    throw { success: false, message: 'article is not found' };
+    throw {
+      success: false,
+      message: 'article is not found',
+    };
   }
 
-  if (article.author_id === decodedId.id) {
+  if (article.author_id === userId) {
     await articles.deleteArticleById(articleId);
-    res.data = { success: true };
+    res.data = {
+      success: true,
+    };
     next();
   } else {
-    throw { success: false, message: 'no rights to delete this article' };
+    throw {
+      success: false,
+      message: 'no rights to delete this article',
+    };
   }
 };
 
 const addComment = async (req, res, next) => {
-  const { params: { id: articleId } } = req;
-  const token = req.headers.authorization.slice(4);
-  const decodedId = await jwt.decode(token);
-  console.log(decodedId);
-  await commentaries.addNewCommentary(req.body.message, articleId, decodedId.id);
-  res.data = { success: true };
+  const {
+    params: {
+      id: articleId,
+    },
+  } = req;
+  const {
+    user: {
+      id: userId,
+    },
+  } = req;
+  await commentaries.addNewCommentary(req.body.message, articleId, userId);
+  res.data = {
+    success: true,
+  };
   next();
 };
 
 const getCommentList = async (req, res, next) => {
-  const { params: { id: articleId } } = req;
-
+  const {
+    params: {
+      id: articleId,
+    },
+  } = req;
   const сomments = await commentaries.getCommentaryList(req.query, articleId);
   res.data = сomments;
   next();
 };
 
 const getComment = async (req, res, next) => {
-  const { params: { id: commentId } } = req;
+  const {
+    params: {
+      id: commentId,
+    },
+  } = req;
   const comment = await commentaries.getCommentaryById(commentId);
   res.data = comment;
   next();
 };
 
 const deleteComment = async (req, res, next) => {
-  const { params: { id: articleId, commentId } } = req;
-  const token = req.headers.authorization.slice(4);
-  const decodedId = await jwt.decode(token);
+  const {
+    params: {
+      id: articleId,
+      commentId,
+    },
+  } = req;
+  const {
+    user: {
+      id: userId,
+    },
+  } = req;
   const article = await articles.findArticleById(articleId);
   const comment = await commentaries.findCommentaryById(commentId);
 
   if (!article) {
-    throw { success: false, message: 'article is not found' };
+    throw {
+      success: false,
+      message: 'article is not found',
+    };
   }
 
   if (!comment) {
-    throw { success: false, message: 'comment is not found' };
+    throw {
+      success: false,
+      message: 'comment is not found',
+    };
   }
 
-  if ((comment.author_id === decodedId.id) || (article.author_id === decodedId.id)) {
-    console.log('lia');
+  if ((comment.author_id === userId) || (article.author_id === userId)) {
     await commentaries.deleteCommentaryById(commentId);
-    res.data = { success: true };
+    res.data = {
+      success: true,
+    };
     next();
   } else {
-    throw { success: false, message: 'no rights to delete this commentary' };
+    throw {
+      success: false,
+      message: 'no rights to delete this commentary',
+    };
   }
 };
 
--- routes/articles.js
+++ routes/articles.js
@@ -2,18 +2,19 @@ const articlesController = require('../controllers/articles');
 const router = require('express-promise-router')();
 const strategy = require('../common/auth_strategy');
 const passport = require('passport');
+const injectionUser = require('../common/injectionUser');
 
 passport.use(strategy);
 
-router.put('/articles/:id', passport.authenticate('jwt', { session: false }), articlesController.editArticle);
-router.post('/articles', passport.authenticate('jwt', { session: false }), articlesController.addArticle);
+router.put('/articles/:id', passport.authenticate('jwt', { session: false }), injectionUser, articlesController.editArticle);
+router.post('/articles', passport.authenticate('jwt', { session: false }), injectionUser, articlesController.addArticle);
 router.get('/articles', articlesController.getPubicArticles);
-router.get('/my', passport.authenticate('jwt', { session: false }), articlesController.getMyArticles);
-router.delete('/articles/:id', passport.authenticate('jwt', { session: false }), articlesController.deleteArticles);
+router.get('/my', passport.authenticate('jwt', { session: false }), injectionUser, articlesController.getMyArticles);
+router.delete('/articles/:id', passport.authenticate('jwt', { session: false }), injectionUser, articlesController.deleteArticles);
 
-router.post('/articles/:id/comments', passport.authenticate('jwt', { session: false }), articlesController.addComment);
+router.post('/articles/:id/comments', passport.authenticate('jwt', { session: false }), injectionUser, articlesController.addComment);
 router.get('/articles/:id/comments', articlesController.getCommentList);
 router.get('/articles/:id/comments/:id', articlesController.getComment);
-router.delete('/articles/:id/comments/:commentId', passport.authenticate('jwt', { session: false }), articlesController.deleteComment);
+router.delete('/articles/:id/comments/:commentId', passport.authenticate('jwt', { session: false }), injectionUser, articlesController.deleteComment);
 
 module.exports = router;
